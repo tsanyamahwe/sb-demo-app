0e6467d566acdb11f554d00a1d200683b168bb3e
Tendai Sanyamahwe <TSanyamahwe@dynamicdna.co.za>
2025-05-23 11:53:09 +0200

    sbdemoapp

--- src/main/java/com/dailycodework/sbdemoapp/view/ProductService.java
+++ src/main/java/com/dailycodework/sbdemoapp/view/ProductService.java
@@ -4,6 +4,9 @@ import com.dailycodework.sbdemoapp.model.Product;
 import com.dailycodework.sbdemoapp.repository.ProductRepository;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
@@ -11,15 +14,22 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class ProductService {
+@Component
+public class ProductService implements CommandLineRunner {
+    @Autowired
     private final ProductRepository productRepository;
 
-    private final List<Product> products = Arrays.asList(
-            new Product(1L, "Samsung", 100, "Some smart phones"),
-            new Product(2L, "iPhone", 500, "Some smart phones"),
-            new Product(3L, "Sneakers", 70, "Some smart shoes"),
-            new Product(4L, "Dell", 200, "Some smart laptops"),
-            new Product(5L, "HP", 100, "Some smart computer"));
+    @Override
+    public void run(String... args) throws Exception {
+        final List<Product> products = Arrays.asList(
+                new Product(1L, "Samsung", 100, "Some smart phones"),
+                new Product(2L, "iPhone", 500, "Some smart phones"),
+                new Product(3L, "Sneakers", 70, "Some smart shoes"),
+                new Product(4L, "Dell", 200, "Some smart laptops"),
+                new Product(5L, "HP", 100, "Some smart computer"));
+
+        productRepository.saveAll(products);
+    }
 
     public List<Product> getProducts(){
         return productRepository.findAll();
